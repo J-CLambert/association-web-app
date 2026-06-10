be2586f589f8cdfd52a35cbb5fa1a0e011348f9b
Jam <jam@jumpyone.ch>
2026-06-10 13:42:34 +0200

    build(chore): generate event-api from start.spring.io

---
+++ event-api/src/main/java/com/guymontag/eventapi/EventApiApplication.java
@@ -0,0 +1,13 @@
+package com.guymontag.eventapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventApiApplication.class, args);
+	}
+
+}
