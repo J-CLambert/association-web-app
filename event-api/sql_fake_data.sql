b43f5046ed51337529c008346cb60b92037ba907
Jam <jam@jumpyone.ch>
2026-06-23 16:46:04 +0200

    db(sql_fake_data.sql): scrpit to populate the db in basic SQL

---
+++ event-api/sql_fake_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO events (name,start_of_event,duration,description,status,location)
+VALUES
+('meeting','2026-09-09 03:14:07',30,'brainstorming for festival of music','PLANNED','Geneva'),
+('moving house','2038-01-09 03:14:07',350,'move from local X to local Y','PLANNED','Lausanne'),
+('lottery','2028-01-09 03:14:00',180,'bottom trawl','PLANNED','Geneva'),
+('meeting','2027-01-09 18:14:00',30,'brainstorming for festival of color','PLANNED','Fribourg'),
+('lottery','2026-01-09 20:14:00',180,'bottom trawl','CANCELED','Geneva'),
+('workshop','2024-01-09 08:14:00',180,'formation on gender-based violence','COMPLETED','Geneva'),
+('meeting','2026-01-09 13:30:00',30,'brainstorming for new web site','COMPLETED','Bale'),
+('lottery','2027-01-09 19:30:00',180,'bottom trawl','PLANNED','Geneva'),
+('workshop','2026-10-09 08:15:00',180,'formation on gender-based violence','PLANNED','Lugano');
+
