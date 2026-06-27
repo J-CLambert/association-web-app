ced2ded90eb68ae58f8794ed52407afdf553d1e3
Jam <jam@jumpyone.ch>
2026-06-27 12:19:23 +0200

    test(PageTest): test numinal case basic edges cases

---
+++ event-api/src/test/java/com/guymontag/eventapi/PageTest.java
@@ -0,0 +1,64 @@
+package com.guymontag.eventapi;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class PageTest {
+
+    @Test
+    void shouldReturnNumberElementsInPageWhenGetNumElements() {
+
+        //Arrange
+        int pageNumber = 0;
+
+        int numberObjectsExcepted = 15;
+
+        int maxSize = 20;
+
+        List<Object> listOf20Element = new ArrayList<>(20);
+
+        for (int i = 0; i <= numberObjectsExcepted - 1; i++) {
+            listOf20Element.add(new Object());
+        }
+
+        Page<Object> page = new Page<Object>(listOf20Element, pageNumber, maxSize);
+
+        //Action
+        int numberElementsResult = page.getNumberElements();
+
+        //Assert
+        assertEquals(numberObjectsExcepted, numberElementsResult);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenListGivenIsBiggerThanMaxSize() {
+        //Arrange
+        int pageNumber = 0;
+
+        int numberOfElements = 35;
+
+        int maxSize = 20;
+
+        List<Object> listOf20Element = new ArrayList<>(20);
+
+        for (int i = 0; i <= numberOfElements - 1; i++) {
+            listOf20Element.add(new Object());
+        }
+        PageSizeOutOfBoundException pageSizeOutOfBoundExceptionExcepted =
+                new PageSizeOutOfBoundException("List given is bigger than maxSize");
+
+        //Action
+        PageSizeOutOfBoundException pageSizeOutOfBoundExceptionResult =
+                assertThrows(PageSizeOutOfBoundException.class, () -> new Page<Object>(listOf20Element, pageNumber, maxSize));
+
+        //Assert
+        assertEquals(pageSizeOutOfBoundExceptionExcepted.getClass(), pageSizeOutOfBoundExceptionResult.getClass());
+        assertEquals(pageSizeOutOfBoundExceptionExcepted.getMessage(), pageSizeOutOfBoundExceptionResult.getMessage());
+    }
+
+}
