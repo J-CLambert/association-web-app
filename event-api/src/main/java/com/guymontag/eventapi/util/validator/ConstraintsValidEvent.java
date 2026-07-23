0d770127ebbe42f3f7c1b8b055a53d35521c7203
Jam <jam@jumpyone.ch>
2026-07-23 14:56:06 +0200

    validation(validatorImpl): start to implement constraints

--- event-api/src/main/java/com/guymontag/eventapi/util/validator/ConstraintsValidEvent.java
+++ event-api/src/main/java/com/guymontag/eventapi/util/validator/ConstraintsValidEvent.java
@@ -1,20 +1,31 @@
 package com.guymontag.eventapi.util.validator;
 
 import com.guymontag.eventapi.exception.ApiException;
+import com.guymontag.eventapi.exception.PastEventPlannedException;
 import com.guymontag.eventapi.model.dto.EventDTO;
+import com.guymontag.eventapi.util.EventStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 
-import java.util.HashMap;
+import java.time.Clock;
+import java.time.Instant;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Predicate;
 
+@Configuration
 public class ConstraintsValidEvent {
-    private Map<Predicate<EventDTO>, ApiException>tempConstraints  = Map.of(
-            Predicate<EventDTO>
-    )
 
+    private Clock clock;
 
-    public Map<Predicate<EventDTO>, ApiException> getTemConstraints(){
-        return
+    ConstraintsValidEvent(Clock clock) {
+        this.clock = clock;
     }
+
+    public List<EventConstraint> constraints = List.of(
+            new EventConstraint(
+                    eventDTO -> eventDTO.getStartOfEvent().isBefore(Instant.now(clock)) && (eventDTO.getStatus() == EventStatus.PLANNED),
+                    () -> new PastEventPlannedException("Event cannot be planned and be in the past")),
+            new EventConstraint(
+                    eventDTO -> eventDTO.getStartOfEvent().isAfter(Instant.now(clock)) && (eventDTO.getStatus() == EventStatus.COMPLETED),
+                    () -> new PastEventPlannedException("Event cannot be completed and be in the future"))
+    );
 }
